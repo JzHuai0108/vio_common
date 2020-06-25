cdc13a0ec73286d825c31b388bdc42dbbbed5fce
Yukai Lin <linyukai@outlook.com>
2020-06-25 16:28:25 +0800

    complete tumvi camera time offset

--- python/get_tumvi_camera_time_offset.py
+++ python/get_tumvi_camera_time_offset.py
@@ -8,8 +8,9 @@ find the camera time offset applied to images in image topics, /cam0/image_raw,
 from __future__ import print_function
 
 import numpy as np
-# import rosbag
+import rosbag
 import rospy
+import argparse
 
 
 def example_rospy_time_in_array():
@@ -31,26 +32,41 @@ def get_timestamps_of_topic(bagfile, topic):
         times.appennd(time)
     """
     print("reading from {} at topic {}".format(bagfile, topic))
-    headerstamps = np.array(
-        [rospy.Time(2, 432985),
-         rospy.Time(213908, 324342)])
-    times = np.array([rospy.Time(1, 432985), rospy.Time(213907, 900324342)])
+    headerstamps = []
+    times = []
+    in_bag = rosbag.Bag(bagfile, "r")
+    for _, msg, t in in_bag.read_messages(topics=[topic]):
+        headerstamps.append(msg.header.stamp.to_nsec())
+        times.append(t.to_nsec())
+    in_bag.close()
+
+    headerstamps = np.array(headerstamps)
+    times = np.array(times)
     return headerstamps, times
 
 
 def main():
-    rawbag = ''
-    calibratedbag = ''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rawbag", help="Raw bag.")
+    parser.add_argument("--calibratedbag", help="Calibrated bag.")
+    args = parser.parse_args()
+    rawbag = args.rawbag
+    calibratedbag = args.calibratedbag
     bagfile_list = [rawbag, calibratedbag]
     topic_list = ['/cam0/image_raw', '/cam1/image_raw', '/imu0']
 
-    headerstamps = None
-    times = None
+    headerstamps_all = dict()
+    times_all = dict()
     for bagfile in bagfile_list:
+        headerstamps_all[bagfile] = dict()
+        times_all[bagfile] = dict()
         for topic in topic_list:
             headerstamps, times = get_timestamps_of_topic(bagfile, topic)
+            print('# headerstamps {} # times {}'.format(len(headerstamps), len(times)))
+            headerstamps_all[bagfile][topic] = headerstamps
+            times_all[bagfile][topic] = times
 
-    print('# headerstamps {} # times {}'.format(len(headerstamps), len(times)))
+
     # start analysis of the timestamps
 
     # assert the times and header stamps are not equal for topics in raw bag,
@@ -58,6 +74,10 @@ def main():
     # for topic in topic_list:
     #   assert topic raw header stamps != topic raw times
     #   assert topic calibrated header stamps == topic calibrated times
+
+    for topic in topic_list:
+        assert (abs(headerstamps_all[rawbag][topic] - times_all[rawbag][topic])>1e5).all()
+        assert (abs(headerstamps_all[calibratedbag][topic] - times_all[calibratedbag][topic])<1e5).all()
 
     # The below relations should hold for header stamps of these topics.
     # cam0 raw header stamp + cam0 time delay + overall time shift =
@@ -70,7 +90,10 @@ def main():
     # assert cam0 calibrated times - cam0 raw times != const
     # assert cam1 calibrated times - cam1 raw times != const
     # assert imu0 calibrated times - imu0 raw times != const
-
+    for topic in topic_list:
+        #print(times_all[calibratedbag][topic] - times_all[rawbag][topic])
+        diff = times_all[calibratedbag][topic] - times_all[rawbag][topic]
+        assert (diff[1:] != diff[0]).all()
     # cam0 calibrated header stamp - cam0 raw header stamp  = const deltac0
     # cam1 calibrated header stamp - cam1 raw header stamp  = const deltac1
     # imu0 calibrated header stamp - imu0 raw header stamp = overall_time_shift
@@ -79,11 +102,19 @@ def main():
     # assert deltac0 == const
     # assert deltac1 == const
     # assert overall_time_shift == const
+    for topic in topic_list:
+        #print(headerstamps_all[calibratedbag][topic] - headerstamps_all[rawbag][topic])
+        diff = headerstamps_all[calibratedbag][topic] - headerstamps_all[rawbag][topic]
+        assert (diff[1:] == diff[0]).all()
+
+    deltac0 = headerstamps_all[calibratedbag]['/cam0/image_raw'][0] - headerstamps_all[rawbag]['/cam0/image_raw'][0]
+    deltac1 = headerstamps_all[calibratedbag]['/cam1/image_raw'][0] - headerstamps_all[rawbag]['/cam1/image_raw'][0]
+    overall_time_shift = headerstamps_all[calibratedbag]['/imu0'][0] - headerstamps_all[rawbag]['/imu0'][0]
 
     # cam0_time_delay = deltac0 - overall_time_shift
     # cam1_time_delay = deltac1 - overall_time_shift
-    cam0_time_delay = 0
-    cam1_time_delay = 0
+    cam0_time_delay = deltac0 - overall_time_shift
+    cam1_time_delay = deltac1 - overall_time_shift
 
     print('Time delay for rosbag {}'.format(bagfile_list[0]))
     print('cam {}: {}'.format(0, cam0_time_delay))
