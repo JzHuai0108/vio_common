53dfdd627703ebc845635d1c757097ab012cab9a
JIanzhu Huai <huai.3@osu.edu>
2020-06-29 11:18:08 +0800

    tumvi raw sequences have essential the same timestamps as calibrated ones

--- python/get_tumvi_camera_time_offset.py
+++ python/get_tumvi_camera_time_offset.py
@@ -7,10 +7,11 @@ find the camera time offset applied to images in image topics, /cam0/image_raw,
 """
 from __future__ import print_function
 
+import argparse
+
 import numpy as np
 import rosbag
 import rospy
-import argparse
 
 
 def example_rospy_time_in_array():
@@ -47,8 +48,8 @@ def get_timestamps_of_topic(bagfile, topic):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--rawbag", help="Raw bag.")
-    parser.add_argument("--calibratedbag", help="Calibrated bag.")
+    parser.add_argument("rawbag", help="Raw bag.")
+    parser.add_argument("calibratedbag", help="Calibrated bag.")
     args = parser.parse_args()
     rawbag = args.rawbag
     calibratedbag = args.calibratedbag
@@ -62,10 +63,10 @@ def main():
         times_all[bagfile] = dict()
         for topic in topic_list:
             headerstamps, times = get_timestamps_of_topic(bagfile, topic)
-            print('# headerstamps {} # times {}'.format(len(headerstamps), len(times)))
+            print('# headerstamps {} # times {}'.format(
+                len(headerstamps), len(times)))
             headerstamps_all[bagfile][topic] = headerstamps
             times_all[bagfile][topic] = times
-
 
     # start analysis of the timestamps
 
@@ -76,8 +77,10 @@ def main():
     #   assert topic calibrated header stamps == topic calibrated times
 
     for topic in topic_list:
-        assert (abs(headerstamps_all[rawbag][topic] - times_all[rawbag][topic])>1e5).all()
-        assert (abs(headerstamps_all[calibratedbag][topic] - times_all[calibratedbag][topic])<1e5).all()
+        assert (abs(headerstamps_all[rawbag][topic] - times_all[rawbag][topic])
+                > 1e5).all()
+        assert (abs(headerstamps_all[calibratedbag][topic] -
+                    times_all[calibratedbag][topic]) < 1e3).all()
 
     # The below relations should hold for header stamps of these topics.
     # cam0 raw header stamp + cam0 time delay + overall time shift =
@@ -91,7 +94,7 @@ def main():
     # assert cam1 calibrated times - cam1 raw times != const
     # assert imu0 calibrated times - imu0 raw times != const
     for topic in topic_list:
-        #print(times_all[calibratedbag][topic] - times_all[rawbag][topic])
+        # print(times_all[calibratedbag][topic] - times_all[rawbag][topic])
         diff = times_all[calibratedbag][topic] - times_all[rawbag][topic]
         assert (diff[1:] != diff[0]).all()
     # cam0 calibrated header stamp - cam0 raw header stamp  = const deltac0
@@ -103,13 +106,18 @@ def main():
     # assert deltac1 == const
     # assert overall_time_shift == const
     for topic in topic_list:
-        #print(headerstamps_all[calibratedbag][topic] - headerstamps_all[rawbag][topic])
-        diff = headerstamps_all[calibratedbag][topic] - headerstamps_all[rawbag][topic]
+        # print(headerstamps_all[calibratedbag][topic] -
+        #     headerstamps_all[rawbag][topic])
+        diff = headerstamps_all[calibratedbag][topic] - headerstamps_all[
+            rawbag][topic]
         assert (diff[1:] == diff[0]).all()
 
-    deltac0 = headerstamps_all[calibratedbag]['/cam0/image_raw'][0] - headerstamps_all[rawbag]['/cam0/image_raw'][0]
-    deltac1 = headerstamps_all[calibratedbag]['/cam1/image_raw'][0] - headerstamps_all[rawbag]['/cam1/image_raw'][0]
-    overall_time_shift = headerstamps_all[calibratedbag]['/imu0'][0] - headerstamps_all[rawbag]['/imu0'][0]
+    deltac0 = headerstamps_all[calibratedbag]['/cam0/image_raw'][
+        0] - headerstamps_all[rawbag]['/cam0/image_raw'][0]
+    deltac1 = headerstamps_all[calibratedbag]['/cam1/image_raw'][
+        0] - headerstamps_all[rawbag]['/cam1/image_raw'][0]
+    overall_time_shift = headerstamps_all[calibratedbag]['/imu0'][
+        0] - headerstamps_all[rawbag]['/imu0'][0]
 
     # cam0_time_delay = deltac0 - overall_time_shift
     # cam1_time_delay = deltac1 - overall_time_shift
