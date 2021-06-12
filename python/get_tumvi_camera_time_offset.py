134df984dcbb175df0d0c7004929975200a74e44
jhuai <huai.3@osu.edu>
2021-06-12 20:25:03 +0800

    simplify get tumvi camera time offset

--- python/get_tumvi_camera_time_offset.py
+++ python/get_tumvi_camera_time_offset.py
@@ -22,7 +22,7 @@ def example_rospy_time_in_array():
     assert c[1].to_nsec() == 1e8
 
 
-def get_timestamps_of_topic(bagfile, topic):
+def get_timestamps_of_topic(bagfile, topic, max_samples):
     """
 
     :param bagfile:
@@ -32,11 +32,9 @@ def get_timestamps_of_topic(bagfile, topic):
         headerstamps.append(msg.header.stamp)
         times.appennd(time)
     """
-    print("reading from {} at topic {}".format(bagfile, topic))
     headerstamps = []
     times = []
     in_bag = rosbag.Bag(bagfile, "r")
-    max_samples = 100
     for _, msg, t in in_bag.read_messages(topics=[topic]):
         headerstamps.append(msg.header.stamp.to_nsec())
         times.append(t.to_nsec())
@@ -58,82 +56,58 @@ def main():
     rawbag = args.rawbag
     calibratedbag = args.calibratedbag
     bagfile_list = [rawbag, calibratedbag]
-    topic_list = ['/cam0/image_raw', '/cam1/image_raw', '/imu0']
+    topic_list = ['/cam0/image_raw', '/cam1/image_raw', '/imu0', '/vrpn_client/raw_transform']
 
     headerstamps_all = dict()
     times_all = dict()
+    max_samples = 200
     for bagfile in bagfile_list:
         headerstamps_all[bagfile] = dict()
         times_all[bagfile] = dict()
         for topic in topic_list:
-            headerstamps, times = get_timestamps_of_topic(bagfile, topic)
-            print('# headerstamps {} # times {}'.format(
-                len(headerstamps), len(times)))
+            headerstamps, times = get_timestamps_of_topic(bagfile, topic, max_samples)
+            print('Check #header.stamps {} #message times {} of topic {} in {}'.format(
+                len(headerstamps), len(times), topic, bagfile))
             headerstamps_all[bagfile][topic] = headerstamps
             times_all[bagfile][topic] = times
 
-    # start analysis of the timestamps
-
-    # assert the times and header stamps are not equal for topics in raw bag,
+    # assert that times and header stamps are not equal for topics in raw bag,
     # but are equal for topics in calibrated bag.
-    # for topic in topic_list:
-    #   assert topic raw header stamps != topic raw times
-    #   assert topic calibrated header stamps == topic calibrated times
-
     for topic in topic_list:
         assert (abs(headerstamps_all[rawbag][topic] - times_all[rawbag][topic])
                 > 1e5).all()
         assert (abs(headerstamps_all[calibratedbag][topic] -
                     times_all[calibratedbag][topic]) < 1e3).all()
 
-    # The below relations should hold for header stamps of these topics.
-    # cam0 raw header stamp + cam0 time delay + overall time shift =
-    #     cam0 calibrated header stamp
-    # cam1 raw header stamp + cam1 time delay + overall time shift =
-    #     cam1 calibrated header stamp
-    # imu0 raw header stamp + overall time shift = imu0 calibrated header stamp
-
-    # assert the raw times are not consistent.
-    # assert cam0 calibrated times - cam0 raw times != const
-    # assert cam1 calibrated times - cam1 raw times != const
-    # assert imu0 calibrated times - imu0 raw times != const
+    # assert calibrated times - raw times != const
     for topic in topic_list:
-        # print(times_all[calibratedbag][topic] - times_all[rawbag][topic])
         diff = times_all[calibratedbag][topic] - times_all[rawbag][topic]
         assert (diff[1:] != diff[0]).all()
-    # cam0 calibrated header stamp - cam0 raw header stamp  = const deltac0
-    # cam1 calibrated header stamp - cam1 raw header stamp  = const deltac1
-    # imu0 calibrated header stamp - imu0 raw header stamp = overall_time_shift
 
-    # assert the below for header stamps,
-    # assert deltac0 == const
-    # assert deltac1 == const
-    # assert overall_time_shift == const
-    for topic in topic_list:
-        # print(headerstamps_all[calibratedbag][topic] -
-        #     headerstamps_all[rawbag][topic])
+    # calibrated header stamps - raw header stamps  = const time_shift.
+    print('Time delay for topics in rosbag {}'.format(bagfile_list[0]))
+    print('time delay + raw.header.stamp = calibrated.header.stamp.')
+    timedelays = {}
+    for index, topic in enumerate(topic_list):
         diff = headerstamps_all[calibratedbag][topic] - headerstamps_all[
             rawbag][topic]
-        assert (diff[1:] == diff[0]).all()
+        if index < 3:
+            assert (diff[1:] == diff[0]).all()
+        else:
+            assert max(abs(diff[1:] - diff[0])) < 1100
 
-    deltac0 = headerstamps_all[calibratedbag]['/cam0/image_raw'][
-        0] - headerstamps_all[rawbag]['/cam0/image_raw'][0]
-    deltac1 = headerstamps_all[calibratedbag]['/cam1/image_raw'][
-        0] - headerstamps_all[rawbag]['/cam1/image_raw'][0]
-    overall_time_shift = headerstamps_all[calibratedbag]['/imu0'][
-        0] - headerstamps_all[rawbag]['/imu0'][0]
-
-    # cam0_time_delay = deltac0 - overall_time_shift
-    # cam1_time_delay = deltac1 - overall_time_shift
-    cam0_time_delay = deltac0 - overall_time_shift
-    cam1_time_delay = deltac1 - overall_time_shift
-
-    print('Time delay for rosbag {}'.format(bagfile_list[0]))
-    print('cam {}: {}'.format(0, cam0_time_delay))
-    print('cam {}: {}'.format(1, cam1_time_delay))
-    print('{} time delay: {}, such that td + raw.header.stamp = '
-          'calibrated.header.stamp.'.format(rawbag, overall_time_shift))
-
+        delay = headerstamps_all[calibratedbag][topic][0] - headerstamps_all[rawbag][topic][0]
+        timedelays[topic] = delay
+        print('{}: {}'.format(topic, delay))
+    if timedelays['/cam0/image_raw'] == timedelays['/cam1/image_raw'] and \
+            timedelays['/cam0/image_raw'] == timedelays['/imu0']:
+        print('Stereo cameras and IMU topics have the same time offset.')
+    else:
+        print('Stereo cameras and IMU topics do NOT have the same time offset.')
+    print('IMU calibrated.header.stamp + {} = mocap calibrated.header.stamp'.format(
+        timedelays['/vrpn_client/raw_transform'] - timedelays['/imu0']))
+    print('Warn: Do not use the estimated time delay between mocap and IMU which '
+        'is slightly different from values given by the authors.')
 
 if __name__ == '__main__':
     main()
