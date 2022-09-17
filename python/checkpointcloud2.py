2857e6ba9425411517839e268814c7c01c62f59f
Jianzhu Huai <huai.3@osu.edu>
2022-09-17 11:31:20 +0800

    check point cloud2

---
+++ python/checkpointcloud2.py
@@ -0,0 +1,64 @@
+import sensor_msgs.point_cloud2 as pc2
+
+import rosbag
+
+import sys
+
+if len(sys.argv) < 3:
+    print('Usage: {} <bagname> <topic> [sensor(velodyne, hesai, ouster)]'.format(sys.argv[0]))
+    exit(1)
+
+# pcl datatypes
+bagname = sys.argv[1]
+pointcloudtopic = sys.argv[2]
+sensor = ''
+if len(sys.argv) >= 4:
+    sensor = sys.argv[3]
+else:
+    if 'velodyne' in pointcloudtopic:
+        sensor = 'velodyne'
+    elif 'hesai' in pointcloudtopic:
+        sensor = 'hesai'
+    elif 'ouster' in pointcloudtopic:
+        sensor = 'ouster'
+    else:
+        sensor = sys.argv[2]
+
+bag = rosbag.Bag(bagname)
+i = 0
+for topic, msg, t in bag.read_messages(topics=[pointcloudtopic]):
+    if i == 0:
+        print('Pointcloud2 fields: {}'.format(msg.fields))
+
+    print('{}, header time: {}, ros time: {}'.format(i, msg.header.stamp, t))
+    j = 0
+    if sensor == 'velodyne':
+        velodyne_gen = pc2.read_points(msg, field_names=("x", "y", "z", "intensity", "ring", "time"), skip_nans=True)
+        # For every point in velodyne pointcloud2, its timestamp in seconds + message header timestamp is its firing time..
+        for p in velodyne_gen:
+            print(" x : %f  y: %f  z: %f intensity: %f ring: %d time %f sec" % (p[0], p[1], p[2], p[3], p[4], p[5]))
+            j += 1
+            if j > 5:
+                break
+    elif sensor == 'hesai':
+        hesai_gen = pc2.read_points(msg, field_names=("x", "y", "z", "intensity", "timestamp", "ring"), skip_nans=True)
+        # For every point in hesai pointcloud2, its timestamp in seconds is its actual firing time.
+        for p in hesai_gen:
+            print(" x : %f  y: %f  z: %f intensity: %f ring: %d time %f sec" % (p[0], p[1], p[2], p[3], p[5], p[4]))
+            j += 1
+            if j > 5:
+                break
+    elif sensor == 'points_raw':
+        velodyne_raw_gen = pc2.read_points(msg, field_names=("x", "y", "z", "time", "ring"), skip_nans=True)
+        for p in velodyne_raw_gen:
+            print(" x : %f  y: %f  z: %f ring: %d time %f us" % (p[0], p[1], p[2], p[4], p[3]))
+            j += 1
+            if j > 5:
+                break
+    # livox lidar uses a custom message type http://docs.ros.org/en/kinetic/api/livox_ros_driver/html/msg/CustomMsg.html
+    # its fields are demoed here https://github.com/hku-mars/FAST_LIO/blob/cd49bd8af90253cd0fe64edce07d1eaa72354b41/src/preprocess.cpp
+    i += 1
+    if i > 5:
+        break
+
+bag.close()
