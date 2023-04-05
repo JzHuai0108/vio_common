a56b8e203cff32fd541857c5c9b891b7860d1590
Jianzhu Huai <huai.3@osu.edu>
2023-04-05 17:02:30 +0800

    check tumvi's maplab vimap

--- test/test_vimap_container.cpp
+++ test/test_vimap_container.cpp
@@ -2,7 +2,8 @@
  * file: test_vimap_container.cpp
  */
 #include "gtest/gtest.h"
-
+#include <glog/logging.h>
+#include "kannala_brandt_camera4.hpp"
 #include "vio/VimapContainer.h"
 
 TEST(VimapContainer, createValidKeypoints) {
@@ -84,4 +85,90 @@ TEST(VimapContainer, createValidKeypoints) {
         << "Actual:" << actualKeypoints << "\nExpected:" << expectedKeypoints
         << "\n";
   }
+}
+
+TEST(VimapContainer, ProjectionCheck) {
+  std::string file_path = __FILE__;
+  std::string vio_common_test_dir =
+      file_path.substr(0, file_path.find_last_of("/\\"));
+
+  Eigen::Matrix<double, 3, 4> T_BC0;
+  T_BC0 << -0.999525037869675, 0.00750191850740521, -0.0298901303164331, 0.0455748356496981,
+          0.0296153438858632, -0.0343973606139315, -0.998969345370176, -0.0711618018379971,
+                -0.00852232821165475, -0.999380079249884,  0.0341588512738562, -0.0446812541171444;
+  Eigen::Vector3d p_BC0 = T_BC0.col(3);
+  Eigen::Quaterniond q_BC0(T_BC0.topLeftCorner<3, 3>());
+  Eigen::Matrix<double, 8, 1> c0;
+  c0 << 190.97847715128717, 190.9733070521226, 254.93170605935475, 256.8974428996504, 
+  0.0034823894022493434, 0.0007150348452162257, -0.0020532361418706202, 0.00020293673591811182;
+  basalt::KannalaBrandtCamera4<double> kb0(c0);
+
+  Eigen::Matrix<double, 3, 4> T_BC1;
+  T_BC1 <<  -0.999511048497858, 0.00810407926382252, -0.0301991362458914, -0.0554563391425955,
+                  0.0302991163766006, 0.0125116437201924, -0.999462566741855, -0.0692500216422477,
+                 -0.00772188302873336, -0.999888885162099, -0.0127510725739409, -0.0474528648075134;
+  
+  Eigen::Vector3d p_BC1 = T_BC1.col(3);
+  std::cout << "p_BC1 " << p_BC1.transpose() << std::endl;
+  Eigen::Quaterniond q_BC1(T_BC1.topLeftCorner<3, 3>());
+  std::cout << "q_BC1 " << q_BC1.coeffs().transpose() << std::endl;
+
+  Eigen::Matrix<double, 8, 1> c1;
+  c1 << 190.44236969414825, 190.4344384721956, 252.59949716835982, 254.91723064636983, 
+  0.0034003170790442797, 0.001766278153469831, -0.00266312569781606, 0.0003299517423931039;
+
+  basalt::KannalaBrandtCamera4<double> kb1(c1);
+
+  std::string vimap_folder = "/data/maplab/maplab_tumviroom1/301cc02fcde84c171300000000000000";
+  vio::VimapContainer vimap(vimap_folder);
+  std::cout << "number of cameras " << vimap.numberCameras() << std::endl;
+  // compute projection for each keypoint using the equidistant model
+  // borrow the model from basalt.
+  const std::vector<vio::CornersInImage, Eigen::aligned_allocator<vio::CornersInImage>>&
+    keypointFrames = vimap.validKeypoints();
+  const std::vector<vio::MaplabLandmarkPattern, Eigen::aligned_allocator<vio::MaplabLandmarkPattern>>& 
+    landmarks = vimap.landmarks();
+  const std::vector<vio::MaplabVertexPattern, Eigen::aligned_allocator<vio::MaplabVertexPattern>>&
+    vertices = vimap.vertices();
+  std::vector<double> reprojerrors;
+  reprojerrors.reserve(10000);
+  for (size_t k = 0; k < landmarks.size(); k++) {
+      CHECK(k == landmarks[k].landmark_index);
+  }
+  size_t good = 0, bad = 0, left = 0, right = 0;
+  for (size_t i = 0; i < keypointFrames.size(); ++i) {
+    for (size_t j = 0; j < keypointFrames[i].corners.size(); j++) {
+      Eigen::Vector2d mz = keypointFrames[i].corners[j];
+      size_t lmkId = keypointFrames[i].corner_ids[j];
+      Eigen::Vector3d point = landmarks[lmkId].position;
+
+      size_t vertexid = i / vimap.numberCameras();
+      size_t camid = i % vimap.numberCameras();
+      Eigen::Vector3d p_WB = vertices[vertexid].p_WS_;
+      Eigen::Quaterniond q_WB = vertices[vertexid].q_WS_;
+      Eigen::Vector3d pointB = q_WB.inverse() * (point - p_WB);
+
+      Eigen::Matrix<double, 2, 1> predicted_z;
+      if (camid == 0) {
+        Eigen::Vector3d pointC = q_BC0.inverse() * (pointB - p_BC0);
+        kb0.project(pointC, predicted_z, nullptr, nullptr);
+        left++;
+      } else {
+        Eigen::Vector3d pointC = q_BC1.inverse() * (pointB - p_BC1);
+        kb1.project(pointC, predicted_z, nullptr, nullptr);
+        right++;
+      }
+      
+      Eigen::Matrix<double, 2, 1> res = predicted_z - mz;
+      
+      if(res.lpNorm<Eigen::Infinity>() > 5){
+        std::cout<< "warn: reproj res " << res.transpose() << std::endl;
+        bad++;
+      } else {
+        good++;
+      }
+      reprojerrors.push_back(res.norm());
+    }
+  }
+  std::cout << "Good " << good << " bad " << bad << " left " << left << " right " << right << std::endl;
 }
