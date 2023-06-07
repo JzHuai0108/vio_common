9db239af2421d564667009bf336ad38446c46ae7
jhuai <huai.3@osu.edu>
2023-06-07 18:41:38 +0800

    add bag_to_pose

---
+++ python/bag_to_pose.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python2
+# copied from https://github.com/JzHuai0108/rpg_trajectory_evaluation
+import os
+import argparse
+
+import rosbag
+
+
+def extract(bagfile, pose_topic, msg_type, out_filename):
+    n = 0
+    f = open(out_filename, 'w')
+    f.write('# timestamp tx ty tz qx qy qz qw\n')
+    with rosbag.Bag(bagfile, 'r') as bag:
+        for (topic, msg, ts) in bag.read_messages(topics=str(pose_topic)):
+            if msg_type == "PoseWithCovarianceStamped":
+                f.write('%.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' %
+                        (msg.header.stamp.to_sec(),
+                         msg.pose.pose.position.x, msg.pose.pose.position.y,
+                         msg.pose.pose.position.z,
+                         msg.pose.pose.orientation.x,
+                         msg.pose.pose.orientation.y,
+                         msg.pose.pose.orientation.z,
+                         msg.pose.pose.orientation.w))
+            elif msg_type == "PoseStamped":
+                f.write('%.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' %
+                        (msg.header.stamp.to_sec(),
+                         msg.pose.position.x, msg.pose.position.y,
+                         msg.pose.position.z,
+                         msg.pose.orientation.x, msg.pose.orientation.y,
+                         msg.pose.orientation.z, msg.pose.orientation.w))
+            elif msg_type == "TransformStamped":
+                f.write('%.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f\n' %
+                        (msg.header.stamp.to_sec(),
+                         msg.transform.translation.x, msg.transform.translation.y,
+                         msg.transform.translation.z,
+                         msg.transform.rotation.x, msg.transform.rotation.y,
+                         msg.transform.rotation.z, msg.transform.rotation.w))
+            elif msg_type == "Odometry":
+                f.write('%.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.8f %.8f %.8f\n' %
+                        (msg.header.stamp.to_sec(),
+                         msg.pose.pose.position.x, msg.pose.pose.position.y,
+                         msg.pose.pose.position.z,
+                         msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
+                         msg.pose.pose.orientation.z, msg.pose.pose.orientation.w, 
+                         msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z))
+            elif msg_type == "Imu":
+                f.write('%.12f %.8f %.8f %.8f %.8f %.8f %.8f\n' %
+                        (msg.header.stamp.to_sec(),
+                         msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z,
+                         msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z))
+            else:
+                assert False, "Unknown message type"
+            n += 1
+    print('wrote ' + str(n) + ' messages to the file: ' + out_filename)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''
+    Extracts IMU messages from bagfile.
+    ''')
+    parser.add_argument('bag', help='Bagfile')
+    parser.add_argument('topic', help='Topic')
+    parser.add_argument('--msg_type', default='PoseStamped',
+                        help='message type')
+    parser.add_argument('--output', default='stamped_poses.txt',
+                        help='output filename')
+    args = parser.parse_args()
+
+    out_dir = os.path.dirname(os.path.abspath(args.bag))
+    out_fn = os.path.join(out_dir, args.output)
+
+    print('Extract pose from bag '+args.bag+' in topic ' + args.topic)
+    print('Saving to file '+out_fn)
+    extract(args.bag, args.topic, args.msg_type, out_fn)
